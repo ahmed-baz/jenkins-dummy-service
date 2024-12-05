c52c5548a43028b95183f16e13f8db880892f381
Ahmed Baz <developer.baz@gmail.com>
2024-12-05 21:42:58 +0200

    set tomcat config

--- src/main/java/org/demo/app/DummyService.java
+++ src/main/java/org/demo/app/DummyService.java
@@ -2,12 +2,19 @@ package org.demo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DummyService {
+public class DummyService extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DummyService.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DummyService.class);
     }
 
 }
